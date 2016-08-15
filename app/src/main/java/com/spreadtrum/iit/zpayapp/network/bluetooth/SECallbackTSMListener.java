6ffed4b1d7330926b13e0060e0f49dd3cfdeb459
SPREADTRUM\ting.long <ting.long@spreadtrum.com>
2016-08-15 08:48:06 +0800

    TSM到SE数据通路，但连接TSM仍然不稳定

---
+++ app/src/main/java/com/spreadtrum/iit/zpayapp/network/bluetooth/SECallbackTSMListener.java
@@ -0,0 +1,8 @@
+package com.spreadtrum.iit.zpayapp.network.bluetooth;
+
+/**
+ * Created by SPREADTRUM\ting.long on 16-8-12.
+ */
+public interface SECallbackTSMListener{
+    void callbackTSM(byte[] responseData,int responseLen);
+}
